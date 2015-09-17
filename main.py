a359e9457e6792dd8b249aad194251b92d066909
暮晨 <362864550@qq.com>
2015-09-17 15:09:17 +0800

    增加漏洞盒子的任务,修改wooyun获取时间为5分钟

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import sched
 import fix360
 import WooYun
+import freebuf
 from Common import mail
 from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -11,6 +12,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 scheduled = sched.scheduler( time.time , time.sleep )
 robot_360 = fix360.fix360('KeyWords.txt' , './Events/EventsID360.txt')
 robot_WooYun = WooYun.WooYun('KeyWords.txt' , './Events/EventsID.txt')
+robot_FreeBuf = freebuf.FreeBuf('KeyWords.txt' , './Events/EventsIDFreeBuf.txt')
 robot_timereport = mail.MailCreate('运行报告')
 
 def run360():
@@ -22,6 +24,11 @@ def runWooYun():
     print('*'*15+'WooYun start Running! The time is: %s' % datetime.now())
     robot_WooYun.keyWordscheck(robot_WooYun.dataAchieve(robot_WooYun.dataRequest()))
     print('*'*15+'WooYun Running over! The time is: %s' % datetime.now())
+
+def runFreeBuf():
+    print('*'*15+'FreeBuf start Running! The time is: %s' % datetime.now())
+    robot_FreeBuf.keyWordscheck(robot_FreeBuf.dataAchieve(robot_FreeBuf.dataRequest()))
+    print('*'*15+'FreeBuf Running over! The time is: %s' % datetime.now())
 
 def runtimeStart():
     print('*'*15+'Program Start Report! The time is: %s'% datetime.now())
@@ -37,12 +44,14 @@ def Start():
     runtimeStart()
     run360()
     runWooYun()
+    runFreeBuf()
 
 if __name__ == '__main__':
     Start()
     scheduler = BackgroundScheduler()
     scheduler.add_job(run360,'interval', seconds=1800)
-    scheduler.add_job(runWooYun,'interval', seconds=60)
+    scheduler.add_job(runWooYun,'interval', seconds=300)
+    scheduler.add_job(runFreeBuf,'interval', seconds=3600)
     scheduler.add_job(runtimeReport,'interval', seconds=43200)
     scheduler.start()
     try:
