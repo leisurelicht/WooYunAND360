54fc2c167e8671329f9f1830e309f34195cc4ae2
暮晨 <362864550@qq.com>
2015-12-26 20:11:34 +0800

    根据PEP8进行了规范

--- Common/filehandle.py
+++ Common/filehandle.py
@@ -1,176 +1,148 @@
 #! usr/bin/env python
 # -*- coding=utf-8 -*-
-
 import os
-import sys
 import json
 import logging
 import hashlib
 import mail
+from common import *
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 logging.basicConfig()
 
 
+def events_id_check(new_id, events_id_list):
+    """
+    通过ID确定事件是否已经被发送过
+    返回一个list
+    如果list里包含0,则已被发送过
+    :param new_id: 新事件的ID
+    :param events_id_list: 事件的ID集合
+    """
+    print 'events_id_check'
+    temp = []
+    if len(events_id_list) > 0:
+        for eventId in events_id_list:
+            if not eventId.isspace():
+                temp.append(cmp(eventId, new_id))
+    return temp
+
+
 class FileHandle(mail.MailCreate):
     """docstring for FileHandle"""
-    def __init__(self, sendername,keysfile , eventsIdfile):
-        super(FileHandle, self).__init__(sendername)
-        self.keyfile = keysfile
-        self.eventsIdfile = eventsIdfile
 
-    def eventsIdread(self):
-        '''
+    def __init__(self, sender_name, keys_file, events_id_file):
+        super(FileHandle, self).__init__(sender_name)
+        self.key_file = keys_file
+        self.events_Id_file = events_id_file
+
+    def events_id_read(self):
+        """
         从文件中读取已经发送过邮件的事件ID
         返回一个list
-        '''
-        print "eventsIdread"
-        errorId = []
-        if os.path.exists( self.eventsIdfile ):
-            with open( self.eventsIdfile ) as errors:
+        """
+        print "events_id_read"
+        error_id = []
+        if os.path.exists(self.events_Id_file):
+            with open(self.events_Id_file) as errors:
                 for error in errors:
                     if not error.isspace():
-                        errorId.append( error.strip() )
-            return errorId
+                        error_id.append(error.strip())
+            return error_id
         else:
-            filepath , filename = os.path.split( self.eventsIdfile )
-            if not os.path.exists( filepath ):
-                os.makedirs( filepath )
+            file_path, file_name = os.path.split(self.events_Id_file)
+            if not os.path.exists(file_path):
+                os.makedirs(file_path)
             else:
-                tmp = open( self.eventsIdfile,'a' )
+                tmp = open(self.events_Id_file, 'a')
                 tmp.close()
 
-
-    def eventsIdadd(self,newId):
-        '''
+    def events_id_add(self, new_id):
+        """
         向EventsID.txt文件中加入一个事件ID
-        返回一个list
-        '''
-        tmp = open( self.eventsIdfile,'a' )
-        tmp.write(newId+'\n')
+        :param new_id: 事件ID
+        """
+        tmp = open(self.events_Id_file, 'a')
+        tmp.write(new_id + '\n')
         tmp.close()
 
-
-    def keyWordsread(self):
-        '''
+    @property
+    def key_words_read(self):
+        """
         从文件中读取需要监看的关键字
         返回一个json格式的数据
-        '''
-        print 'keyWordsread'
-        if os.path.exists(self.keyfile):
-            with open(self.keyfile) as keys:
+        """
+        print 'key_words_read'
+        if os.path.exists(self.key_file):
+            with open(self.key_file) as keys:
                 tmp = keys.read()
             try:
                 keywordslist = json.loads(tmp)
             except Exception as e:
-                errortext = "Error in functon : \" %s \" ,\n \
-                Error name is : \" %s \" ,\n \
-                Error type is : \" %s \" ,\n \
-                Error Message is : \" %s \" ,\n \
-                Error doc is : \" %s \" , \n " % \
-                (sys._getframe().f_code.co_name,
-                 e.__class__.__name__,
-                 e.__class__,
-                 e,
-                 e.__class__.__doc__)
-                self.sendTextEmail("Program Exception",errortext,"ExceptionInfo")
-            return keywordslist
+                error_text = exception_format(e)
+                self.sendTextEmail("Program Exception", error_text, "ExceptionInfo")
+            else:
+                return keywordslist
         else:
             print "关键词监看文件不存在"
-            tmp = open(self.keyfile, 'a')
+            tmp = open(self.key_file, 'a')
             tmp.close()
 
-
-    def fileMd5get(self):
-        '''
+    @property
+    def file_md5_get(self):
+        """
         获取文件的MD5值
         返回一个MD5值
-        '''
-        print 'fileMd5get'
+        """
+        print 'file_md5_get'
         try:
-            filemd5 = hashlib.md5()
-            with open(self.keyfile) as filetemp:
-                filemd5.update( filetemp.read().strip() )
-                md5temp = filemd5.hexdigest()
+            file_md5 = hashlib.md5()
+            with open(self.key_file) as filetemp:
+                file_md5.update(filetemp.read().strip())
+                md5temp = file_md5.hexdigest()
         except Exception as e:
-            errortext = "Error in functon : \" %s \" ,\n \
-            Error name is : \" %s \" ,\n \
-            Error type is : \" %s \" ,\n \
-            Error Message is : \" %s \" ,\n \
-            Error doc is : \" %s \" , \n " % \
-            (sys._getframe().f_code.co_name,
-             e.__class__.__name__,
-             e.__class__,
-             e,
-             e.__class__.__doc__)
-            #print errortext
-            self.sendTextEmail("Program Exception",errortext,"ExceptionInfo")
+            error_text = exception_format(e)
+            self.sendTextEmail("Program Exception", error_text, "ExceptionInfo")
         else:
             return md5temp
 
-    def fileMd5check(self,oldfilemd5):
-        '''
+    def file_md5_check(self, old_file_md5):
+        """
         检查文件的MD5值,确定文件是否发生改变
         没有发生改变返回False
         发生改变返回新的MD5值
-        '''
-        print 'fileMd5check'
+        :param old_file_md5:
+        """
+        print 'file_md5_check'
         try:
-            #filemd5 = hashlib.md5()
-            #with open(self.keyfile) as filetemp:
+            # filemd5 = hashlib.md5()
+            # with open(self.keyfile) as filetemp:
             #    filemd5.update( filetemp.read().strip() )
             #    md5temp = filemd5.hexdigest()
-            md5temp = self.fileMd5get()
+            md5temp = self.file_md5_get()
         except Exception as e:
-            errortext = "Error in functon : \" %s \" ,\n \
-            Error name is : \" %s \" ,\n \
-            Error type is : \" %s \" ,\n \
-            Error Message is : \" %s \" ,\n \
-            Error doc is : \" %s \" , \n " % \
-            (sys._getframe().f_code.co_name,
-             e.__class__.__name__,
-             e.__class__,
-             e,
-             e.__class__.__doc__)
-            #print errortext
-            self.sendTextEmail("Program Exception",errortext,"ExceptionInfo")
+            error_text = exception_format(e)
+            self.sendTextEmail("Program Exception", error_text, "ExceptionInfo")
         else:
-            if oldfilemd5 == md5temp:
+            if old_file_md5 == md5temp:
                 return False
             else:
                 return md5temp
-
-    def eventsIdCheck(self,newId,eventsIdlist):
-        '''
-        通过ID确定事件是否已经被发送过
-        返回一个list
-        如果list里包含0,则已被发送过
-        '''
-        print 'eventsIdCheck'
-        temp = []
-        if ( len(eventsIdlist) > 0 ):
-            for eventId in eventsIdlist:
-                 if not eventId.isspace():
-                    temp.append( cmp( eventId,newId ) )
-        return temp
-
-
+        return False
 
 if __name__ == '__main__':
-    test = FileHandle('filehandle','KeyWords.txt' , '../Events/EventsID360.txt')
-    #print test.eventsIdread()
-    #for key in test.keyWordsread():
+    test = FileHandle('filehandle', 'KeyWords.txt', '../Events/EventsID360.txt')
+    # print test.eventsIdread()
+    # for key in test.key_words_read():
     #    print key
-    #a=test.fileMd5get()
-    #print a
+    # a=test.file_md5_get()
+    # print a
 
-    #while True:
-    #    b=test.fileMd5check(a)
+    # while True:
+    #    b=test.file_md5_check(a)
     #    print b
     #    if b:
     #        a = b
     #    time.sleep(5)
-    print test.eventsIdread()
-
-
+    print test.events_id_read()
