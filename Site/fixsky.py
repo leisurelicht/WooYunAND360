41466199fad4121cd95d86ae708f1eaba7dd7829
暮晨 <362864550@qq.com>
2016-01-02 17:31:51 +0800

    修改了文件md5值的检查部分

--- Site/fixsky.py
+++ Site/fixsky.py
@@ -20,7 +20,7 @@ class FixSky(filehandle.FileHandle, mail.MailCreate):
         self._360fix_url = 'http://loudong.360.cn/vul/list'
         self._360base_url = 'http://loudong.360.cn'
         self.events_id_list = self.events_id_read()
-        self.key_word_list = self.key_words_read
+        self.key_words_list = self.key_words_read
         self.fileMd5 = self.file_md5_get
         self.html = 0
         self.count = 0
@@ -100,14 +100,15 @@ class FixSky(filehandle.FileHandle, mail.MailCreate):
         :param events:
         """
         print '360_key_words_check'
-        temp_file_md5 = self.file_md5_check(self.fileMd5)
-        if temp_file_md5:
-            self.fileMd5 = temp_file_md5
-            self.key_word_list = self.key_words_read
+        md5value = self.file_md5_get
+        if md5value != self.fileMd5:
+            self.key_words_list = self.key_words_read
+            self.fileMd5 = md5value
         try:
             for (_360url, _360title) in events.iteritems():
+                _360title = _360title.lower()
                 # print _360title
-                for key1, values in self.key_word_list.iteritems():
+                for key1, values in self.key_words_list.iteritems():
                     if key1 in _360title:
                         if values:
                             for value in values:
@@ -115,16 +116,22 @@ class FixSky(filehandle.FileHandle, mail.MailCreate):
                                 if value.get('KEY2') is not None:
                                     if value.get('KEY2') in _360title:
                                         # print '1.',_360title
-                                        self.send_record(_360title, self._360base_url + _360url, _360url.split('/')[-1])
+                                        self.send_record(_360title,
+                                                         self._360base_url + _360url,
+                                                         _360url.split('/')[-1])
                                     # else: #二级关键词不中的话继续查域名和内容
                                         # 2. 进入页面检查厂商域名
                                         # 3. 在页面内查找是否存在第二关键字
                                 elif value.get('KEY2') is None:
                                     # print '3.',_360title
-                                    self.send_record(_360title, self._360base_url + _360url, _360url.split('/')[-1])
+                                    self.send_record(_360title,
+                                                     self._360base_url + _360url,
+                                                     _360url.split('/')[-1])
                         else:
                             # print '2.',_360title
-                            self.send_record(_360title, self._360base_url + _360url, _360url.split('/')[-1])
+                            self.send_record(_360title,
+                                             self._360base_url + _360url,
+                                             _360url.split('/')[-1])
         except Exception as e:
             error_text = exception_format(get_current_function_name(), e)
             self.send_text_email('Program Exception', error_text, 'ExceptionInfo')
--- Site/freebuf.py
+++ Site/freebuf.py
@@ -20,7 +20,7 @@ class FreeBuf(filehandle.FileHandle, mail.MailCreate):
         self.freebuf_url = 'https://www.vulbox.com/board/internet/page/'
         self.freebuf_base_url = 'https://www.vulbox.com'
         self.events_id_list = self.events_id_read()
-        self.key_word_list = self.key_words_read
+        self.key_words_list = self.key_words_read
         self.fileMd5 = self.file_md5_get
         self.html = 0
         # self.count = 0
@@ -96,33 +96,37 @@ class FreeBuf(filehandle.FileHandle, mail.MailCreate):
         没有返回值
         :param events:
         """
-        print 'freebuf_keyWordscheck'
-        temp_file_md5 = self.file_md5_check(self.fileMd5)
-        if temp_file_md5:
-            self.fileMd5 = temp_file_md5
-            self.key_word_list = self.key_words_read
+        print 'freebuf_keyWords_check'
+        md5value = self.file_md5_get
+        if md5value != self.fileMd5:
+            self.key_words_list = self.key_words_read
+            self.fileMd5 = md5value
         try:
             for (freebuf_url, freebuf_title) in events.iteritems():
+                freebuf_title = freebuf_title.lower()
                 # print freebuf_title
-                for key1, values in self.key_word_list.iteritems():
+                for key1, values in self.key_words_list.iteritems():
                     if key1 in freebuf_title:
                         if values:
                             for value in values:
                                 if value.get('KEY2') is not None:
                                     if value.get('KEY2') in freebuf_title:
                                         # print '1',freebuf_title
-                                        self.send_record(freebuf_title, self.freebuf_base_url+freebuf_url,
+                                        self.send_record(freebuf_title,
+                                                         self.freebuf_base_url+freebuf_url,
                                                          freebuf_url.split('/')[-1])
                                     # else: #二级关键词不中的话继续查域名和内容
                                         # 2. 进入页面检查厂商域名
                                         # 3. 在页面内查找是否存在第二关键字
                                 elif value.get('KEY2') is None:
                                     # print '3',freebuf_title
-                                    self.send_record(freebuf_title, self.freebuf_base_url + freebuf_url,
+                                    self.send_record(freebuf_title,
+                                                     self.freebuf_base_url + freebuf_url,
                                                      freebuf_url.split('/')[-1])
                         else:
                             # print '2',freebuf_title
-                            self.send_record(freebuf_title, self.freebuf_base_url+freebuf_url,
+                            self.send_record(freebuf_title,
+                                             self.freebuf_base_url+freebuf_url,
                                              freebuf_url.split('/')[-1])
         except Exception as e:
             error_text = exception_format(get_current_function_name(), e)
