39c83e4695c5204702da875f69e31bdd4b4778f5
暮晨 <362864550@qq.com>
2016-01-05 17:16:19 +0800

    自动加入后台

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+#! usr/bin/env python
+# -*- coding=utf-8 -*-
 import os
-from Function.Run import begin
+from Function import Run
 
-os.chdir('./Function')
-begin()
+#
+# pid = os.fork()
+pid1 = os.fork()
+# if pid == 0:
+#     print "进程1启动成功 %s,%s" % (pid, os.getpid())
+    # os.system("python ./WebSite/WebSite.py")
+if pid1 == 0:
+    print "进程2启动成功 %s,%s" % (pid1, os.getpid())
+    os.chdir('./Function/')
+    Run.begin()
+
+print "所有进程启动结束"
