6ebcb453b658bac0ddfcae3519954a36c4d78ed4
暮晨 <362864550@qq.com>
2016-06-01 21:43:31 +0800

    update

--- Common/common.py
+++ Common/common.py
@@ -1,10 +1,11 @@
 #! usr/bin/env python
 # -*- coding=utf-8 -*-
-import sys
+
 import inspect
 
-reload(sys)
-sys.setdefaultencoding('utf8')
+# import sys
+# reload(sys)
+# sys.setdefaultencoding('utf8')
 
 def get_current_function_name():
     """
